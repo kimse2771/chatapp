751b6da514f930a854e5b2b3827d9a4b871cac8b
kse3917 <kws3917@naver.com>
2023-04-04 14:03:18 +0900

    채팅 기능

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/chatting/chat/message.dart';
+import 'package:chatapp/chatting/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -49,35 +51,17 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/6Y5q1QnHVd1r6wRpu9BU/message')
-            .snapshots(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-              itemBuilder: (context,index){
-              return Container(
-                padding:EdgeInsets.all(8.0),
-                child: Text(
-                    docs[index]['text'],
-                  style: TextStyle(fontSize: 20.0),
-                ),
-
-
-              );
-              },
-          );
-        },
-      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded
+              (
+                child: Messages()
+            ),
+            NewMessage(),
+          ],
+        ),
+      )
     );
   }
 }
